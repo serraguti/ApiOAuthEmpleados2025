6bc52a073b661595703b5ebbffabed51bfa16d93
serraguti <pacoserranox@gmail.com>
2025-03-27 13:25:12 +0100

    Add project files.

---
+++ ApiOAuthEmpleados/Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using ApiOAuthEmpleados.Helpers;
+using ApiOAuthEmpleados.Models;
+using ApiOAuthEmpleados.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ApiOAuthEmpleados.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private RepositoryHospital repo;
+        //CUANDO GENEREMOS EL TOKEN DEBEMOS INTEGRAR
+        //ALGUNOS DATOS COMO ISSUER Y DEMAS
+        private HelperActionServicesOAuth helper;
+
+        public AuthController(RepositoryHospital repo
+            , HelperActionServicesOAuth helper)
+        {
+            this.repo = repo;
+            this.helper = helper;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ActionResult> 
+            Login(LoginModel model)
+        {
+            Empleado empleado = await
+                this.repo.LogInEmpleadoAsync(model.UserName
+                , int.Parse(model.Password));
+            if (empleado == null)
+            {
+                return Unauthorized();
+            }
+            else
+            {
+                //DEBEMOS CREAR UNAS CREDENCIALES PARA 
+                //INCLUIRLAS DENTRO DEL TOKEN Y QUE ESTARAN 
+                //COMPUESTAS POR EL SECRET KEY CIFRADO Y EL 
+                //TIPO DE CIFRADO QUE INCLUIREMOS EN EL TOKEN
+                SigningCredentials credentials =
+                    new SigningCredentials
+                    (this.helper.GetKeyToken(),
+                    SecurityAlgorithms.HmacSha256);
+                //EL TOKEN SE GENERA CON UNA CLASE
+                //Y DEBEMOS INDICAR LOS DATOS QUE ALMACENARA EN SU 
+                //INTERIOR
+                JwtSecurityToken token =
+                    new JwtSecurityToken(
+                        issuer: this.helper.Issuer,
+                        audience: this.helper.Audience,
+                        signingCredentials: credentials,
+                        expires: DateTime.UtcNow.AddMinutes(20),
+                        notBefore: DateTime.UtcNow
+                        );
+                //POR ULTIMO, DEVOLVEMOS LA RESPUESTA AFIRMATIVA
+                //CON UN OBJETO QUE CONTENGA EL TOKEN (anonimo)
+                return Ok(new
+                {
+                    response =
+                    new JwtSecurityTokenHandler()
+                    .WriteToken(token)
+                });
+            }
+        }
+    }
+}
---
+++ ApiOAuthEmpleados/Repositories/RepositoryHospital.cs
@@ -0,0 +1,37 @@
+﻿using ApiOAuthEmpleados.Data;
+using ApiOAuthEmpleados.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiOAuthEmpleados.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            return await this.context.Empleados.ToListAsync();
+        }
+
+        public async Task<Empleado> 
+            FindEmpleadoAsync(int idEmpleado)
+        {
+            return await this.context.Empleados
+                .FirstOrDefaultAsync(z => z.IdEmpleado == idEmpleado);
+        }
+
+        public async Task<Empleado>
+            LogInEmpleadoAsync(string apellido, int idEmpleado)
+        {
+            return await this.context.Empleados
+                .Where(x => x.Apellido == apellido
+                && x.IdEmpleado == idEmpleado)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
